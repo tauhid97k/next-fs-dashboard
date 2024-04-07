f356c0410e0f01bb2a997d3f8d0940a004f52a29
tauhid97k <tauhid.coder@gmail.com>
2024-04-08 02:28:27 +0600

    routing middleware for auth, api and public access

--- middleware.ts
+++ middleware.ts
@@ -1,10 +1,39 @@
 import NextAuth from 'next-auth'
 import authConfig from '@/auth.config'
+import {
+  DEFAULT_LOGIN_REDIRECT,
+  apiAuthPrefix,
+  authRoutes,
+  publicRoutes,
+} from '@/auth.routes'
+import { NextResponse } from 'next/server'
+
 const { auth } = NextAuth(authConfig)
 
 export default auth((req) => {
-  const isAuthenticated = Boolean(req.auth)
-  // More codes here...
+  const { nextUrl } = req
+  const isLoggedIn = Boolean(req.auth)
+
+  const isApiAuthRoute = nextUrl.pathname.startsWith(apiAuthPrefix)
+  const isPublicRoute = publicRoutes.includes(nextUrl.pathname)
+  const isAuthRoute = authRoutes.includes(nextUrl.pathname)
+
+  if (isApiAuthRoute) {
+    return NextResponse.next()
+  }
+
+  if (isAuthRoute) {
+    if (isLoggedIn) {
+      return NextResponse.redirect(new URL(DEFAULT_LOGIN_REDIRECT, nextUrl))
+    }
+    return NextResponse.next()
+  }
+
+  if (!isLoggedIn && !isPublicRoute) {
+    return NextResponse.redirect(new URL('/login', nextUrl))
+  }
+
+  return NextResponse.next()
 })
 
 export const config = {
